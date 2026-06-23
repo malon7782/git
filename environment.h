3976e7eb4104fb18b2d099299203885612bbb72e
Tian Yuchen <cat@malon.dev>
2026-06-24 00:44:39 +0800

    commit #1
    migrate the lazy parsing logic of setup_standard_excludes() in dir.c
    into a newly introduced getter in environment.c
    
    the reason for this is that I thought dir.c/setup_standard_excludes()
    might not be the best place to call xdg_config_home(). is it?
    
    Signed-off-by: Tian Yuchen <cat@malon.dev>

--- environment.h
+++ environment.h
@@ -133,6 +133,12 @@ int git_default_config(const char *, const char *,
 int git_default_core_config(const char *var, const char *value,
 			    const struct config_context *ctx, void *cb);
 
+/*
+ * TODO: This still relies on the global state.
+ * It will be moved to repo_config_values in the subsequent commit.
+ */
+const char *repo_excludes_file(struct repository *repo);
+
 void repo_config_values_init(struct repo_config_values *cfg);
 
 /*
